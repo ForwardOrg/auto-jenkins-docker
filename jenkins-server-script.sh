d7f59388d4b4090b7dba9b2222a714a4dc1abbf1
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-07-29 18:01:54 -0700

    Update jenkins-server-script.sh

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -28,3 +28,28 @@ sudo yum -y install terraform
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+
+# install docker
+sudo yum update
+sudo yum search docker
+sudo yum install docker -y
+
+# add the user to the Docker group
+
+sudo usermod -a -G docker ec2-user
+id ec2-user
+
+# Reload a Linux user's group assignments to docker w/o logout
+newgrp docker
+
+sudo dnf upgrade --releasever=2023.1.20230725 -y
+# Add required dependencies for the jenkins package
+sudo su -
+sudo yum install java -y
+
+
+
+# log out and log back in for the group membership to take effect
+
+sudo reboot
